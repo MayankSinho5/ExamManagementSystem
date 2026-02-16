211aa63853d46a88eda1bad1cb2e296a21e8025a
MAYANK <pariharmayank978@gmail.com>
2026-02-16 17:42:24 +0530

    Cleanup: Removed debug error logger after successful Timetable fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-window.onerror = (msg, url, line, col, error) => {
-  alert(`Error: ${msg}\nAt: ${url}:${line}:${col}`);
-};
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
